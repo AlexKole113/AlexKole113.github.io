da7a5e67fc4644a8feec9a7ef2f49c9322805204
AlexKole113 <alexander.koledov@gmail.com>
2021-07-14 11:28:50 +0200

    fix favorites page

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -2,7 +2,7 @@ import menuStyles from '@/components/Menu/styles/index.scss';
 import usePageTitle from '@/hooks/usePageTitle/usePageTitle';
 
 const PageTitle = () => {
-  const title = usePageTitle('Default Title');
+  const title = usePageTitle('Welcome');
   return (<h1 className={menuStyles['page-title']}>{title}</h1>);
 };
 
--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,36 +1,28 @@
-//@ts-nocheck
-import Search from "@/components/Search";
-import GroupedCards from "@/components/GroupedCards";
+// @ts-nocheck
+
+import GroupedCards from '@/components/GroupedCards';
+import useThemeSwitcher from '@/hooks/useThemeSwitcher';
 import favoritesCss from './styles/index.scss';
-import useThemeSwitcher from "@/hooks/useThemeSwitcher";
-
-
-
 
 const Favorites = () => {
-    useThemeSwitcher('favorites-theme' );
-    return (
-        <div className={favoritesCss['shop-group']}>
+  const products = [
+    {
+      id: 131, sku: 131, title: 'Trees 1', image: 'https://assets.codepen.io/2736535/trees-1.jpeg', data: [{ length: '50cm' }], price: 301.99, currency: '$', stock: 20,
+    },
+    {
+      id: 132, sku: 132, title: 'Trees 2 Fav', image: 'https://assets.codepen.io/2736535/trees-2.jpeg', data: [{ length: '50cm' }], price: 302.99, currency: '$', stock: 20,
+    },
+    {
+      id: 133, sku: 133, title: 'Trees 3', image: 'https://assets.codepen.io/2736535/trees-3.jpeg', data: [{ length: '50cm' }], price: 303.99, currency: '$', stock: 20,
+    },
+  ];
 
-            <section className={favoritesCss['shop-group-transition']}>
-                <section
-                    className={`${favoritesCss['shop-group-transition-group']}  ${favoritesCss['shop-group-hidden']}`}>
-
-                    <Search/>
-                    <GroupedCards/>
-
-                </section>
-                <section
-                    className={`${favoritesCss['shop-group-transition-group']} ${favoritesCss['shop-group-current']}`}>
-
-                    <Search/>
-                    <GroupedCards/>
-
-                </section>
-            </section>
-        </div>
-    );
-
-}
+  useThemeSwitcher('favorites-theme');
+  return (
+    <div className={favoritesCss['shop-group']}>
+      <GroupedCards products={products} />
+    </div>
+  );
+};
 
 export default Favorites;
