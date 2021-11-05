2e7f63fd12596ee36b11abfb03bb4ec081a8d4bd
AlexKole113 <alexander.koledov@gmail.com>
2021-11-05 15:13:12 +0100

    Settings page #stage 1

--- src/components/SelectSettings/index.tsx
+++ src/components/SelectSettings/index.tsx
@@ -1,13 +1,17 @@
+import React from "react";
 import selectSettingsCss from './styles/index.scss'
 
 
-const SelectSettings = () => (
-    <select tabIndex={1} className={`${selectSettingsCss['select-control']} ${selectSettingsCss['settings-select']}`} name="" id="language">
-        <option value="eng">&#127468;&#127463;</option>
-        <option value="fr">&#127464;&#127477;</option>
-        <option value="de">&#127465;&#127466;</option>
-        <option value="pirat">&#127988;&#8205;&#9760;&#65039;</option>
-    </select>
-)
+
+const SelectSettings = ({value,options,onSelect}:{value:string, options:{value:string,label:string}[], onSelect:(newValue:string)=>{}}) => {
+    const onChangeHandler = (e:React.FormEvent) => [
+        onSelect((e.target as HTMLSelectElement).value)
+    ]
+    return (
+        <select onChange={onChangeHandler} value={value} tabIndex={1} className={`${selectSettingsCss['select-control']} ${selectSettingsCss['settings-select']}`} name="" id="language">
+            {options.map( ({value:val,label}) =>  <option key={val} value={val}>{label}</option>)}
+        </select>
+    );
+}
 
 export default SelectSettings;
