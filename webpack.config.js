0125f2cf02d70bc09a452971102d071ffaa11a00
AlexKole113 <alexander.koledov@gmail.com>
2023-02-18 14:28:42 +0100

    add patch for git pages

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = function (env, argv) {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: `${ASSETS_DIR}/js/[name].[contenthash].js`,
-      publicPath: isProduction ? '/mobile-shop-app/dist/' : '/',
+      publicPath: isProduction ? 'https://alexkole113.github.io/mobile-shop-app/dist/' : '/',
       assetModuleFilename: `${ASSETS_DIR}/resources/[name].[contenthash][ext]`,
     },
     devServer: {
